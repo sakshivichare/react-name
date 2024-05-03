404bed6cb7f1b2a6db98b5fd5416ab922506a8e5
sakshivichare <sakshivichare808@gmail.com>
2024-05-03 11:26:30 +0530

    initial commit

---
+++ reactproject/src/App.jsx
@@ -0,0 +1,11 @@
+import Hello from "./component/Hello"
+function App() {
+
+  return (
+    <div className="App">
+      <Hello />
+      </div>
+  );
+}
+
+export default App
---
+++ reactproject/src/component/Hello.jsx
@@ -0,0 +1,11 @@
+function Hello(props)
+{
+    return(       
+            <div>
+            <h1>Hello</h1>
+            </div>       
+    );
+
+
+}
+export default Hello;
